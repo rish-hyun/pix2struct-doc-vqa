7951be0d00a1625cf466246aa145c18236f15176
Rishabh Sethi <rishabhsethi238@gmail.com>
2023-08-13 23:03:28 +0530

    Added onnx model conversion methods

--- convert.py
+++ convert.py
@@ -1,5 +1,7 @@
 import os
 import toml
+from glob import glob
+from onnxruntime.quantization import quantize_dynamic, QuantType
 
 
 config = toml.load("config.toml")["MODEL"]
@@ -11,10 +13,9 @@ MODELS_DIR = config["MODELS_DIR"]
 HF_MODEL_DIR = os.path.join(MODELS_DIR, MODEL_NAME)
 
 if not os.path.isfile(os.path.join(HF_MODEL_DIR, "pytorch_model.bin")):
-    command = (
+    os.system(
         f"git clone https://huggingface.co/{ORGANIZATION}/{MODEL_NAME} {HF_MODEL_DIR}"
     )
-    os.system(command)
 
 
 def convert_to_onnx(with_past: bool):
@@ -27,8 +28,17 @@ def convert_to_onnx(with_past: bool):
 
     command = f"optimum-cli export onnx --model {HF_MODEL_DIR} --task {task} {output}"
     os.system(command)
+    return output
+
+
+def quantize_onnx(onnx_model_path: str):
+    for onnx_model in glob(os.path.join(onnx_model_path, "*.onnx")):
+        quantized_model = onnx_model.replace(".onnx", "_quantized.onnx")
+        quantize_dynamic(onnx_model, quantized_model, weight_type=QuantType.QInt8)
 
 
 if __name__ == "__main__":
-    convert_to_onnx(with_past=False)
-    convert_to_onnx(with_past=True)
+    _ = convert_to_onnx(with_past=False)
+    model_path = convert_to_onnx(with_past=True)
+
+    quantize_onnx(model_path)
